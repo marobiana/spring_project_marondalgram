3eda3ca1e0ce62a3663dc322aadb6171423b6245
marobiana <sbr@kakao.com>
2021-05-16 15:13:41 +0900

    프로젝트 초기화

---
+++ src/test/java/com/marobiana/MarondalgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marobiana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarondalgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
